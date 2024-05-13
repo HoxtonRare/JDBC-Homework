9d00f403143e34fbd6509d1ae2775ab9cbed99ea
Ruslan <ruslan24krut@gmail.com>
2024-05-13 12:28:48 +0300

    add JDBC homework

---
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,31 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        UserServiceImpl us = new UserServiceImpl();
+        List<User> list = new ArrayList<>();
+
+        us.createUsersTable();
+
+        us.saveUser("Ruslan", "Kopaliani", (byte) 23);
+        us.saveUser("Danya", "Ivanov", (byte) 32);
+        us.saveUser("Ilya", "Vasiliev", (byte) 21);
+        us.saveUser("Vladimir", "Alekseev", (byte) 40);
+
+        list = us.getAllUsers();
+        System.out.println(list);
+
+        us.cleanUsersTable();
+
+        us.dropUsersTable();
+    }
+}
